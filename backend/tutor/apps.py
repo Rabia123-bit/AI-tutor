5f7d9d9f1fd974e9dbdaa22a6264df1d541c328c
Rabia123-bit <82121318+Rabia123-bit@users.noreply.github.com>
2026-04-18 15:58:47 +0200

    Update apps.py

--- backend/tutor/apps.py
+++ backend/tutor/apps.py
@@ -1,4 +1,17 @@
 from django.apps import AppConfig
+import os
 
 class TutorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'tutor'
+
+    def ready(self):
+        from django.contrib.auth.models import User
+
+        if os.environ.get("CREATE_SUPERUSER", "False") == "True":
+            username = "admin"
+            email = "admin@example.com"
+            password = "admin123"
+
+            if not User.objects.filter(username=username).exists():
+                User.objects.create_superuser(username, email, password)
